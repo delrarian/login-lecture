f44c0f1c3423c29793b9c63e220d656b04ba8451
delrarian <shyfamily@naver.com>
2022-08-01 04:23:39 +0900

    -dom login data

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,14 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+const id = document.querySelector("#id"),
+    pasword = document.querySelector("#password"),
+    btn_login = document.querySelector("button");
+
+btn_login.addEventListener("click", login);
+
+function login(){
+    const req = {
+        id: id.value,
+        pasword: pasword.value
+    };
+}
