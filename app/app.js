73e0aa5905980dcd26aa62c939ee140979738a85
delrarian <shyfamily@naver.com>
2022-08-03 18:27:33 +0900

    morgan'

--- app/app.js
+++ app/app.js
@@ -4,12 +4,15 @@ const { resolveSoa } = require("dns");
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require('dotenv');
-dotenv.config();
+const morgan = require('morgan');
 
 const app = express();
+dotenv.config();
 
+const accessLogStream = require("./src/config/log");
 //routing
 const home = require("./src/routes/home");
+
 
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
@@ -17,6 +20,8 @@ app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
+app.use(morgan("dev"));
+app.use(morgan("common", { stream: accessLogStream }));
 
 app.use("/", home);
 
